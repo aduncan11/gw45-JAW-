91e1d275cb02f114c1c3fc67cd7c9034de9fa940
jia1997 <xiaoyi11788@gmail.com>
2014-04-30 21:48:00 -0400

    sorry i push the wrong thing the first time

--- KingCrab.java
+++ KingCrab.java
@@ -1,12 +1,19 @@
-import info.gridworld.actor.Actor; 
-import info.gridworld.actor.Critter; 
-import info.gridworld.grid.Grid; 
-import info.gridworld.grid.Location; 
- 
-import java.awt.Color; 
-import java.util.ArrayList; 
- 
-public class KingCrab extends CrabCritter { 
+import info.gridworld.grid.*;
+import info.gridworld.actor.*;
+import java.util.ArrayList;
 
-    } 
-} 
+public class KingCrab extends CrabCritter{
+
+    public void processActors(ArrayList<Actor> actors){
+        for (Actor a : actors){
+            int dir = this.getLocation().getDirectionToward( a.getLocation() );
+            Location next = a.getLocation().getAdjacentLocation(dir);
+            
+            if ( this.getGrid().isValid(next) )
+                a.moveTo(next);
+            else
+                a.removeSelfFromGrid();
+        }
+    }
+}
+
--- QuickCrab.java
+++ QuickCrab.java
@@ -1,13 +1,33 @@
-import info.gridworld.actor.Actor; 
-import info.gridworld.actor.Critter; 
-import info.gridworld.grid.Grid; 
-import info.gridworld.grid.Location; 
- 
-import java.awt.Color; 
-import java.util.ArrayList; 
- 
-public class QuickCrab extends CrabCritter { 
- 
-  
-    } 
-} 
+import info.gridworld.grid.*;
+import info.gridworld.actor.*;
+import java.util.ArrayList;
+
+public class QuickCrab extends CrabCritter{
+
+    public ArrayList<Location> getMoveLocations(){
+    
+        ArrayList<Location> result = new ArrayList<Location>();
+        
+        ArrayList<Integer> dirs = new ArrayList<Integer>();
+        dirs.add( this.getDirection() + 90 );
+        dirs.add( this.getDirection() - 90 );
+        
+        for (Integer d : dirs)
+            Location spot = this.getLocation().getAdjacentLocation(d);
+            spot = spot.getAdjacentLocation(d);
+            
+            if ( ( this.getGrid().isValid(spot) )   &&
+                 ( this.getGrid().get(spot) == null )){
+                result.add(spot);
+            }
+        return result;
+    }
+
+    public Location selectMoveLocation(ArrayList<Location> locs){
+        if ( locs.size() < 2 )
+            return this.getLocation();
+        else
+            return locs.get( (int) ( Math.random() * locs.size() ) );
+    }
+}  
+
