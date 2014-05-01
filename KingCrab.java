02236f1c06688a8de33cdbb871d49eff49c07322
jia1997 <xiaoyi11788@gmail.com>
2014-04-30 21:39:05 -0400

    fix

--- KingCrab.java
+++ KingCrab.java
@@ -7,33 +7,6 @@ import java.awt.Color;
 import java.util.ArrayList; 
  
 public class KingCrab extends CrabCritter { 
-    public KingCrab() { 
-        setColor(Color.RED); 
-    } 
- 
-    public int distanceFrom(Location loc1, Location loc2) { 
-        int x1 = loc1.getRow(); 
-        int y1 = loc1.getCol(); 
-        int x2 = loc2.getRow(); 
-        int y2 = loc2.getCol(); 
-        double distance = Math.sqrt((x1 - x2)*(x1 - x2) + (y1 - y2)*(y1 - y2)) + .5; 
-        return (int)Math.floor(distance); 
-    } 
 
-    private boolean moveOneMoreAway(Actor a){ 
-    ArrayList<Location> loc = getGrid().getEmptyAdjacentLocations(a.getLocation()); 
-     
-    for(Location l:locs)
-        if(distanceFrom(getLocation(), l) > 1)
-            a.moveTo(l); 
-            return true; 
-     
-     return false; 
-    } 
-
-    public void processActors(ArrayList<Actor> actors){ 
-        for (Actor a : actors) 
-            if (!moveOneMoreAway(a)) 
-                a.removeSelfFromGrid(); 
     } 
 } 
--- QuickCrab.java
+++ QuickCrab.java
@@ -8,27 +8,6 @@ import java.util.ArrayList;
  
 public class QuickCrab extends CrabCritter { 
  
-    public QuickCrab() { 
-        setColor(Color.YELLOW); 
-     } 
-
-     public ArrayList<Location> getMoveLocations(){ 
-        ArrayList<Location> loc = new ArrayList<Location>(); 
-        Grid g = getGrid();  
-        addIfGoodTwoAwayMove(loc,getDirection() + Location.LEFT); 
-        addIfGoodTwoAwayMove(loc,getDirection() + Location.RIGHT);          
-        if (loc.size() == 0) 
-            return super.getMoveLocations();         
-        return loc; 
-    } 
-
-     private void addIfGoodTwoAwayMove(ArrayList<Location> loc,int dir) { 
-        Grid g = getGrid(); 
-        Location loc = getLocation(); 
-        Location temp = loc.getAdjacentLocation(dir); 
-        if(g.isValid(temp) && g.get(temp) == null)  
-            Location loc1 = temp.getAdjacentLocation(dir); 
-            if(g.isValid(loc1) && g.get(loc1)== null) 
-                loc.add(loc1);         
+  
     } 
 } 
